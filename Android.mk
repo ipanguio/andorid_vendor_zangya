f305bc29ec3cf2c5a40f52861c055df4860544d9
ipanguio <61322307+ipanguio@users.noreply.github.com>
2023-05-07 02:12:52 +0200

    Update Android.mk
    Prevent "build/make/core/base_rules.mk:260: error: vendor/bq/zangya: MODULE.TARGET.SHARED_LIBRARIES.libsdm-disp-vndapis already defined by vendor/bq/sdm660-common."

--- Android.mk
+++ Android.mk
@@ -18,29 +18,29 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter zangya zangyapro,$(TARGET_DEVICE)),)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libgpustats
-LOCAL_MODULE_OWNER := bq
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libgpustats.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libgpustats.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
+# include $(CLEAR_VARS)
+# LOCAL_MODULE := libgpustats
+# LOCAL_MODULE_OWNER := bq
+# LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libgpustats.so
+# LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libgpustats.so
+# LOCAL_MULTILIB := both
+# LOCAL_MODULE_TAGS := optional
+# LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+# LOCAL_MODULE_SUFFIX := .so
+# LOCAL_VENDOR_MODULE := true
+# include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libsdm-disp-vndapis
-LOCAL_MODULE_OWNER := bq
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libsdm-disp-vndapis.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libsdm-disp-vndapis.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
+# include $(CLEAR_VARS)
+# LOCAL_MODULE := libsdm-disp-vndapis
+# LOCAL_MODULE_OWNER := bq
+# LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libsdm-disp-vndapis.so
+# LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libsdm-disp-vndapis.so
+# LOCAL_MULTILIB := both
+# LOCAL_MODULE_TAGS := optional
+# LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+# LOCAL_MODULE_SUFFIX := .so
+# LOCAL_VENDOR_MODULE := true
+# include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := QtiTelephonyService
